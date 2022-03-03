2f8573c405b7339d34addb76bfd8efce1de6fc4d
Abhi Jadhav <abhijadhav9571@gmail.com>
2022-03-03 17:16:21 -0500

    added routes

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from flask_dynamo import Dynamo
 
 app = Flask(__name__)
+
+
+@app.route('/api/CustomerAccount/GetCustomerAccountByAccountNumber', methods=['GET'])
+def retrieveAccountDetails():
+
+
+@app.route('/api/CustomerAccount/OpenCustomerAccount', methods=['POST'])
+def openAccount():
+    req = request.json
+
+
+@app.route('/api/CustomerAccount/CloseCustomerAccount', methods=['POST'])
+def closeAccount():
+    req = request.json
+
+
+@app.route('/api/CustomerAccount/ApplyTransactionToCustomerAsync', methods=['POST'])
+def applyTransaction():
+    req = request.json 
