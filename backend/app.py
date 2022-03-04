0408df372e1c0037c808e0bfde6facaf3d1081cd
Abhi Jadhav <abhijadhav9571@gmail.com>
2022-03-04 11:42:25 -0500

    deploying to EC2

--- backend/app.py
+++ backend/app.py
@@ -2,16 +2,21 @@ from flask import Flask, request
 from flask_dynamo import Dynamo
 from boto3.session import Session()
 
-boto_sess = Session(
-    region_name='us-east-1',
-    aws_access_key_id='example_key_id',
-    aws_secret_access_key='my_super_secret_key'
-)
-app.config['DYNAMO_SESSION'] = boto_sess
+# boto_sess = Session(
+#     region_name='us-east-1',
+#     aws_access_key_id='example_key_id',
+#     aws_secret_access_key='my_super_secret_key'
+# )
+# app.config['DYNAMO_SESSION'] = boto_sess
 
 
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello, world!"
 
 
 @app.route('/api/CustomerAccount/GetCustomerAccountByAccountNumber', methods=['GET'])
@@ -20,7 +25,13 @@ def retrieveAccountDetails():
 
 @app.route('/api/CustomerAccount/OpenCustomerAccount', methods=['POST'])
 def openAccount():
-    req = request.json
+    # req = request.json
+    # dynamo.tables['R3-Account'].put_item(data={
+    #     'username': 'rdegges',
+    #     'first_name': 'Randall',
+    #     'last_name': 'Degges',
+    #     'email': 'r@rdegges.com',
+    # })
 
 
 @app.route('/api/CustomerAccount/CloseCustomerAccount', methods=['POST'])
